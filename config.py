f3ee3e7332f8292686428c88a6f2d2813fbcf791
lewis-dw <lewis@driftworks.com>
2024-10-22 14:29:47 +0100

    added env file for storing the connection variables to db in

--- config.py
+++ config.py
@@ -1,9 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 class Config:
+    # get environment variables
+    username = os.getenv('DB_USERNAME')
+    password = os.getenv('DB_PASSWORD')
+    host = os.getenv('DB_HOST')
+    port = os.getenv('DB_PORT')
+    name = os.getenv('DB_NAME')
+
     # SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:pwd@localhost:5432/testy'
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{username}:{password}@{host}:{port}/{name}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = False
-
-    # postgres
-    # pwd
-    # 5432
