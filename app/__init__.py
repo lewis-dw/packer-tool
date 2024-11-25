ba1e0a2145c321c1e60b8440f4455df474a56213
lewis-dw <lewis@driftworks.com>
2024-11-25 16:36:25 +0000

    every page now follows the layout.html page

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 def create_app():
+    # create app
     app = Flask(__name__)
     app.config.from_object('config.Config')
+
 
     # Import and register blueprints
     from app.default import default
@@ -10,9 +12,18 @@ def create_app():
     app.register_blueprint(default, url_prefix='/')
     app.register_blueprint(orders, url_prefix='/orders')
 
+
     # Global 404 error handler
     @app.errorhandler(404)
     def page_not_found(e):
         return render_template('404.html'), 404
 
+
+    # serve the manifest
+    @app.route('/manifest.json')
+    def manifest():
+        return send_from_directory('static', 'manifest.json')
+
+
+    # return the app
     return app
