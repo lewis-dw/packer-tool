9178540668b06ce654b1ce990bc14392f918f338
lewis-dw <lewis@driftworks.com>
2024-11-28 13:26:19 +0000

    1.2.0 - not currently working but i wanted to update with a version number as i had not been using it even though i added it in as a dunder var i chose 1.2.0 because i have got the odoo order pull in bit working which i consider a major update the 2 is for a sort of randomly guessed minor update as i have now started implementing fedex

---
+++ app/shipping/ups.py
@@ -0,0 +1,9 @@
+"""
+
+Stages for both quote and ship:
+
+1 - load the data into correct payload format
+2 - perform operation and return result
+
+
+"""
--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 # dunder variables
-__version__ = '0.1.0' # semantic versioning [MAJOR|MINOR|PATCH]
+__version__ = '1.2.0' # semantic versioning [MAJOR|MINOR|PATCH]
 __author__ = 'Lewis Rumsby'
 __email__ = 'lewis@driftworks.com'
 
